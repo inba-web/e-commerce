b83905eac4d2cc9ffac873631c8d2c002e21aefd
Inba <inbavarunans@gmail.com>
2026-02-07 15:38:45 +0530

    implemented address schema & seller service

---
+++ backend/src/service/sellerService.js
@@ -0,0 +1,50 @@
+const Address = require("../model/Address");
+const Seller = require("../model/Seller");
+const jwtProvider = require("../utils/jwtProvider");
+
+
+class SellerService{
+
+    async createSeller(sellerData){
+        const existingSeller = await Seller.findOne({email: sellerData.email});
+        if(existingSeller){
+            throw new Error("Email Already Exists");
+        }
+
+        let savedAddress = sellerData.pickupAddress;
+
+        savedAddress = await Address.create(sellerData.pickupAddress);
+
+        const newSeller = new Seller({
+            sellerName:sellerData.name,
+            email:sellerData.email,
+            password:sellerData.password,
+            pickupAddress:savedAddress._id,
+            GSTIN:sellerData.GSTIN,
+            password:sellerData.password,
+            mobile:sellerData.mobile,
+            bankDetails:sellerData.bankDetails,
+            businessDetails:sellerData.businessDetails
+        })
+
+        return await newSeller.save();
+
+    }
+
+    async getSellerProfile(jwt){
+        const email = jwtProvider.getEmailFromjwt(jwt);
+        return this.getSellerByEmail(email);
+    }
+
+    async getSellerByEmail(email){
+        const seller = await Seller.findOne({email})
+
+        if(!seller){
+            throw new Error("Seller not found")
+        }
+
+        return seller;
+    }
+
+    
+}
---
+++ backend/src/utils/jwtProvider.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken")
+
+const secretKey = "poiuytewqasdfghjklnbvcxz"
+
+class JwtProvider{
+
+    constructor(secretKey){
+        this.secretKey = secretKey
+    }
+
+    createJwt(payload){
+        return jwt.sign(payload, this.secretKey, {expiresIn: "24h"})
+    }
+
+    getEmailFromjwt(token){
+        try {
+            const decodedToken = jwt.verify(token, this.secretKey)
+            return decodedToken.email;
+        } catch (error) {
+            throw new Error("Invalid Token")
+        }
+    }
+
+    verifyJwt(token){
+        try {
+            return  jwt.verify(token, this.secretKey);
+        } catch (error) {
+            throw new Error("Invalid Token")
+        }
+    }
+} 
+
+module.exports = new JwtProvider(secretKey)
