9ccb76c10d0af9d0557b55c28802a6dcf8d52f55
Inba <inbavarunans@gmail.com>
2026-02-07 22:08:13 +0530

    implemented otp fun & updated seller service

---
+++ backend/src/controller/SellerController.js
@@ -0,0 +1,112 @@
+const UserRoles = require("../domain/UserRole");
+const VerificationCode = require("../model/VerificationCode");
+const sellerService = require("../service/sellerService");
+const jwtProvider = require("../utils/jwtProvider");
+
+
+class SellerController{
+
+    // Bearer token for 
+
+    async getSellerProfile(req, res){
+        try {
+            const jwt = req.headers.authorization.split(" ")[1];
+            const seller = await sellerService.getSellerProfile(jwt);
+
+            res.json(200).json(seller);
+        } catch (error) {
+            res.status(error instanceof Error ? 404 : 500)
+            .json({message:error.message})
+        }
+    }
+
+    async createSeller(req, res){
+        try {
+            const seller = await sellerService.createSeller(req.body);
+
+            res.json(200).json({message:"seller created successfully"});
+        } catch (error) {
+            res.status(error instanceof Error ? 404 : 500)
+            .json({message:error.message})
+        }
+    }
+
+    async getAllSellers(req, res){
+        try {
+            const status = req.query.status;
+            const seller = await sellerService.getAllSellers(status);
+
+
+            res.json(200).json(seller);
+        } catch (error) {
+            res.status(error instanceof Error ? 404 : 500)
+            .json({message:error.message})
+        }
+    }
+
+    async updateSeller(req, res){
+        try {
+            const existingSeller = await req.seller
+            const seller = await sellerService.updateSeller(existingSeller,req.body)
+
+            res.json(200).json(seller);
+        } catch (error) {
+            res.status(error instanceof Error ? 404 : 500)
+            .json({message:error.message})
+        }
+    }
+
+     async deleteSeller(req, res){
+        try {
+            const seller = await sellerService.deleteSeller(req.params.id);
+            
+            res.json(200).json({message: "Seller deleted successfully"});
+        } catch (error) {
+            res.status(error instanceof Error ? 404 : 500)
+            .json({message:error.message})
+        }
+    }
+
+    async updateSellerAccountStatus(req, res){
+        try {
+            const updatedSeller = await sellerService.updateSellerStatus(req.params.id,
+                req.params.status
+            )
+            res.status(200).json(updatedSeller)
+        } catch (error) {
+            res.status(error instanceof Error ? 404 : 500)
+            .json({message:error.message})
+        }
+    }
+
+
+    async verifyLoginOtp(req, res){
+        try {
+            const {otp, email} = req.body;
+            const seller = await sellerService.getSellerByEmail(email)
+
+            const verificationCode = await VerificationCode.findOne({email});
+
+            if(!verificationCode || verificationCode.otp != otp){
+                throw new Error("Invalid OTP")
+            }
+
+            const token = jwtProvider.createJwt({email});
+
+            const authResponse = {
+                message:"Login Success",
+                jwt:token,
+                role:UserRoles.SELLER
+            }
+
+            return res.status(200).json(authResponse);
+        } catch (error) {
+            res.status(error instanceof Error ? 404 : 500)
+            .json({message:error.message})
+        }
+    }
+
+
+}
+
+module.exports = SellerController;
--- backend/src/service/sellerService.js
+++ backend/src/service/sellerService.js
@@ -2,49 +2,78 @@ const Address = require("../model/Address");
 const Seller = require("../model/Seller");
 const jwtProvider = require("../utils/jwtProvider");
 
-
-class SellerService{
-
-    async createSeller(sellerData){
-        const existingSeller = await Seller.findOne({email: sellerData.email});
-        if(existingSeller){
-            throw new Error("Email Already Exists");
-        }
-
-        let savedAddress = sellerData.pickupAddress;
-
-        savedAddress = await Address.create(sellerData.pickupAddress);
-
-        const newSeller = new Seller({
-            sellerName:sellerData.name,
-            email:sellerData.email,
-            password:sellerData.password,
-            pickupAddress:savedAddress._id,
-            GSTIN:sellerData.GSTIN,
-            password:sellerData.password,
-            mobile:sellerData.mobile,
-            bankDetails:sellerData.bankDetails,
-            businessDetails:sellerData.businessDetails
-        })
-
-        return await newSeller.save();
-
+class SellerService {
+  async createSeller(sellerData) {
+    const existingSeller = await Seller.findOne({ email: sellerData.email });
+    if (existingSeller) {
+      throw new Error("Email Already Exists");
     }
 
-    async getSellerProfile(jwt){
-        const email = jwtProvider.getEmailFromjwt(jwt);
-        return this.getSellerByEmail(email);
+    let savedAddress = sellerData.pickupAddress;
+
+    savedAddress = await Address.create(sellerData.pickupAddress);
+
+    const newSeller = new Seller({
+      sellerName: sellerData.name,
+      email: sellerData.email,
+      password: sellerData.password,
+      pickupAddress: savedAddress._id,
+      GSTIN: sellerData.GSTIN,
+      password: sellerData.password,
+      mobile: sellerData.mobile,
+      bankDetails: sellerData.bankDetails,
+      businessDetails: sellerData.businessDetails,
+    });
+
+    return await newSeller.save();
+  }
+
+  async getSellerProfile(jwt) {
+    const email = jwtProvider.getEmailFromjwt(jwt);
+    return this.getSellerByEmail(email);
+  }
+
+  async getSellerByEmail(email) {
+    const seller = await Seller.findOne({ email });
+
+    if (!seller) {
+      throw new Error("Seller not found");
     }
 
-    async getSellerByEmail(email){
-        const seller = await Seller.findOne({email})
+    return seller;
+  }
 
-        if(!seller){
-            throw new Error("Seller not found")
-        }
-
-        return seller;
+  async getSellerById(id) {
+    const seller = await Seller.findOne(id);
+    if (!seller) {
+      throw new Error("Seller not found");
     }
 
-    
+    return seller;
+  }
+
+  async getAllSellers(status) {
+    return await Seller.find({ accountStatus: status });
+  }
+
+  async updateSeller(existingSeller, sellerData) {
+    return await Seller.findByIdAndUpdate(existingSeller._id, sellerData, {
+      new: true,
+    });
+  }
+
+  async updateSellerStatus(sellerId, status) {
+    return Seller.findByIdAndUpdate(
+      sellerId,
+      { $set: { accountStatus: status } },
+      { new: true },
+    );
+  }
+
+  async deleteSeller(sellerId) {
+    return await Seller.findByIdAndDelete(sellerId);
+  }
+
 }
+
+module.exports = new SellerService();
