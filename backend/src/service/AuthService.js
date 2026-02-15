49ec727c2f674b216095d978dcefd80d6303770a
Inba <inbavarunans@gmail.com>
2026-02-15 19:14:38 +0530

    feat: user model & user auth

--- backend/src/service/AuthService.js
+++ backend/src/service/AuthService.js
@@ -1,7 +1,10 @@
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
 const VerificationCode = require("../model/VerificationCode");
 const generateOTP = require("../utils/generateOTP");
 const sendVerificataionEmail = require("../utils/sendEmail");
 const sellerService = require("./sellerService");
+const jwtProvider = require("../utils/jwtProvider");
 
 class AuthService {
   async sendLoginOTP(email) {
@@ -13,16 +16,16 @@ class AuthService {
       if (!seller) throw new Error("User not found");
     }
 
-    const existingVerificationCode = await VerificationCode.findOne({email});
+    const existingVerificationCode = await VerificationCode.findOne({ email });
 
-    if(existingVerificationCode){
+    if (existingVerificationCode) {
       await VerificationCode.deleteOne({ email });
     }
 
     const otp = generateOTP();
     const verificationCode = new VerificationCode({ otp, email });
     await verificationCode.save();
-    console.log("verification code : "+verificationCode.otp)
+    console.log("verification code : " + verificationCode.otp);
 
     const subject = "INBA MART Login/Signup OTP";
     const body = `Your OTP is ${otp}. Please enter it to complete your login process.`;
@@ -30,6 +33,30 @@ class AuthService {
 
     console.log("OTP saved in DB:", otp);
   }
+
+  async createUser(req) {
+    const { email, fullName } = req;
+
+    let user = await User.findOne({ email });
+
+    if (user) {
+      throw new Error("User already exists with this email");
+    }
+
+    user = new User({
+      email,
+      fullName,
+      password: await bcrypt.hash(12345678, 10),
+    });
+
+    await user.save();
+
+    const cart = new Cart({ user: user._id });
+    await cart.save();
+
+    return jwtProvider.createJwt({ email });
+  }
+
 }
 
 module.exports = new AuthService();
