218a8c5dae2c83c7eff4067c3e06e94f2fea4b80
Inba <inbavarunans@gmail.com>
2026-04-02 22:06:45 +0530

    updated payment service

--- backend/src/service/PaymentService.js
+++ backend/src/service/PaymentService.js
@@ -1,6 +1,29 @@
+const Orders = require('../model/Order.js');
+const PaymentOrder = require('../model/PaymentOrder.js');
+
 class PaymentService{
     
     async createOrder(user, order){
-        const amount = orders.reduce((sum,order) => sum+order.totalSellingPrice, 0)
+        const amount = Orders.reduce((sum,order) => sum+order.totalSellingPrice, 0)
+
+        const paymentOrder = new PaymentOrder({
+            amount,
+            user:user._id,
+            orders:Orders.map(order => order._id)
+        })
+
+        return await paymentOrder.save();
     }
+
+    async getPaymentOrderById(orderId){
+        const paymentOrder = await PaymentOrder.findOne(orderId);
+
+        if(!paymentOrder){
+            throw new Error("Payment order not found");
+        }
+
+        return paymentOrder;
+    } 
+
+
 }
