8744b025a9ae397faa52002deaddffd97ece4a37
Inba <inbavarunans@gmail.com>
2026-04-03 11:38:09 +0530

    completed paymentservice

--- backend/src/service/PaymentService.js
+++ backend/src/service/PaymentService.js
@@ -1,64 +1,107 @@
-const Orders = require('../model/Order.js');
-const PaymentOrder = require('../model/PaymentOrder.js');
+const OrderStatus = require("../domain/OrderStatus.js");
+const PaymentStatus = require("../domain/PaymentStatus.js");
+const Orders = require("../model/Order.js");
+const PaymentOrder = require("../model/PaymentOrder.js");
 
-class PaymentService{
-    
-    async createOrder(user, order){
-        const amount = Orders.reduce((sum,order) => sum+order.totalSellingPrice, 0)
+class PaymentService {
+  async createOrder(user, order) {
+    const amount = Orders.reduce(
+      (sum, order) => sum + order.totalSellingPrice,
+      0,
+    );
 
-        const paymentOrder = new PaymentOrder({
-            amount,
-            user:user._id,
-            orders:Orders.map(order => order._id)
-        })
+    const paymentOrder = new PaymentOrder({
+      amount,
+      user: user._id,
+      orders: Orders.map((order) => order._id),
+    });
 
-        return await paymentOrder.save();
+    return await paymentOrder.save();
+  }
+
+  async getPaymentOrderById(orderId) {
+    const paymentOrder = await PaymentOrder.findOne(orderId);
+
+    if (!paymentOrder) {
+      throw new Error("Payment order not found");
     }
 
-    async getPaymentOrderById(orderId) {
-        const paymentOrder = await PaymentOrder.findOne(orderId);
+    return paymentOrder;
+  }
 
-        if(!paymentOrder) {
-            throw new Error("Payment order not found");
-        }
+  async getPaymentOrderByPaymentLinkId(paymentLinkId) {
+    const paymentOrder = await PaymentOrder.findOne({ paymentLinkId });
 
-        return paymentOrder;
-    } 
-
-    async getPaymentOrderByPaymentLinkId(paymentLinkId){
-        const paymentOrder = await PaymentOrder.findOne({paymentLinkId});
-
-        if(!paymentOrder){
-            throw new Error("Payment order not found");
-        }
-
-        return paymentOrder;
+    if (!paymentOrder) {
+      throw new Error("Payment order not found");
     }
 
-    async createRazorpayPaymentLink(user, amount, orderId){
-        try {
-            const paymentLinkRequest = {
-                amount: amount*100,
-                currency: "INR",
-                customer:{
-                    name: user.fullName,
-                    email: user.email,
-                },
+    return paymentOrder;
+  }
 
-                notify:{
-                    email: true
-                },
+  async proceedPaymentOrder(paymentOrder, paymentId, paymentLinkId){
 
-                callback_url: `http://localhost:3000/payment-success/${orderId}`,
-                callback_method: "get"
-            }
+    if(paymentOrder.status !== PaymentStatus.PENDING){
+        const payment = await razorpay.payment.fetch(paymentId);
 
-            const paymentLink = await razorpay.paymentLink.create(paymentLinkRequest);
+        if(payment.status === "captured"){
+            await Promisea.all(paymentOrder.orders.map(async (orderId) => {
+                const order = await Orders.findById(orderId);
+                order.paymentStatus = PaymentStatus.COMPLETED;
+                order.orderStatus = OrderStatus.PLACED;
+                await order.save();
+            }));
 
-            return paymentLink;
-        } catch (error) {
-            
-        }
+            paymentOrder.status = PaymentStatus.COMPLETED;
+            await paymentOrder.save(); 
+
+            return true;
+        }else{
+            paymentOrder.status = PaymentStatus.FAILED;
+            await paymentOrder.save();
+
+            await Promise.all(paymentOrder.orders.map(async (orderId) => {
+                const order = await Orders.findById(orderId);
+                order.paymentStatus = PaymentStatus.FAILED;
+                order.orderStatus = OrderStatus.CANCELLED;
+                await order.save();
+            }));
+
+            return false;
+        } 
+
     }
 
+    return false;
+  } 
+
+  async createRazorpayPaymentLink(user, amount, orderId) {
+    try {
+      const paymentLinkRequest = {
+        amount: amount * 100, 
+        currency: "INR",
+        customer: {
+          name: user.fullName,
+          email: user.email,
+        },
+
+        notify: {
+          email: true,
+        },
+
+        callback_url: `http://localhost:3000/payment-success/${orderId}`,
+        callback_method: "get",
+        expire_by: Math.floor(Date.now()/1000) + (60*60) 
+      };
+
+      const paymentLink = await razorpay.paymentLink.create(paymentLinkRequest);
+
+      return paymentLink;
+    } catch (error) {
+      console.error("Razorpay payment link error:", error);
+      throw new Error("Unable to create payment link");
+    }
+  }
 }
+
+module.exports = new PaymentService();
