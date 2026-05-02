3c87d1a19304d034a9f2373c75f83fe275cadfbf
Inba <inbavarunans@gmail.com>
2026-05-02 13:13:08 +0530

    feat: updated Home page with Deal Cards

--- frontend/src/customer/pages/Home/Home.tsx
+++ frontend/src/customer/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import ElectronicCategory from './ElectronicCategory/ElectronicCategory'
 import Grid from './Grid/Grid'
 import Deal from '../Deal/Deal'
+import HomeCategory from './HomeCategory/HomeCategory'
 
 const Home = () => {
   return (
@@ -11,9 +12,14 @@ const Home = () => {
         <Grid />
       </section>
 
-      <section className='mt-10'>
+      <section className='pt-10'>
         <h1 className='text-3xl font-black text-center pb-5'>Today's Deal</h1>
         <Deal />
+      </section>
+
+      <section className='pt-10'>
+        <h1 className='text-3xl font-black text-center pb-5'>Shop By Category</h1>
+        <HomeCategory />
       </section>
 
     </div>
